5bf3531e4797afb4fd138d5bc92275bb78418f12
Daniel Laserna <daniel.laserna@protonmail.com>
2021-05-03 22:24:33 -0400

    Add chokidar and lodash.debounce

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
+const chokidar = require("chokidar");
+const debounce = require("lodash.debounce")
 
-console.log('I was executed');
+const start = debounce(() => {
+  console.log('STARTING USERS PROGRAM');
+}, 100)
+
+chokidar
+  .watch(".")
+  .on("add", start)
+  .on("change", () => console.log("FILE CHANGED"))
+  .on("unlink", () => console.log("FILE UNLINKED"));
