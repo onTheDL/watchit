4eb3ba6ae9b4dda2484896e3bc4d4d8e967f8392
Daniel Laserna <daniel.laserna@protonmail.com>
2021-05-04 21:46:31 -0400

    Implement cli tool with caporal

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 #!/usr/bin/env node
 const chokidar = require("chokidar");
-const debounce = require("lodash.debounce")
+const debounce = require("lodash.debounce");
+const program = require("caporal");
 
-const start = debounce(() => {
-  console.log('STARTING USERS PROGRAM');
-}, 100)
+program
+  .version("0.0.1")
+  .argument("[filename]", "Name of file to execute")
+  .action((args) => {
+    console.log(args)
+  });
 
-chokidar
-  .watch(".")
-  .on("add", start)
-  .on("change", () => console.log("FILE CHANGED"))
-  .on("unlink", () => console.log("FILE UNLINKED"));
+program.parse(process.argv)
+
+// const start = debounce(() => {
+//   console.log("STARTING USERS PROGRAM");
+// }, 100);
+
+// chokidar
+//   .watch(".")
+//   .on("add", start)
+//   .on("change", () => console.log("FILE CHANGED"))
+//   .on("unlink", () => console.log("FILE UNLINKED"));
